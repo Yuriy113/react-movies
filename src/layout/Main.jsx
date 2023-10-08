0aedcab4330fdf3f31f51cf5b75d4ee12aece033
Yuriy Ulanov <1859697@gmail.com>
2023-10-08 13:33:51 +0300

    feat: add env variable

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -4,19 +4,21 @@ import MovieList from '../components/MovieList';
 import Preloader from '../components/Preloader';
 import { Search } from '../components/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends React.Component {
   state = {
     movies: [],
   };
 
   fetchMovies = (searchString = 'matrix') => {
-    fetch(`http://www.omdbapi.com/?apikey=2049fdfd&s=${searchString}`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchString}`)
       .then((res) => res.json())
       .then((data) => this.setState({ movies: data.Search }));
   };
 
   filterMovies = (searchString = 'matrix', filterString) => {
-    fetch(`http://www.omdbapi.com/?apikey=2049fdfd&s=${searchString}&type=${filterString}`)
+    fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${searchString}&type=${filterString}`)
       .then((res) => res.json())
       .then((data) => this.setState({ movies: data.Search }));
   };
